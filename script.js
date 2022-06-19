17c0bdd26ad711ff8760354b919b73c4c4cfed0d
vigan7x <52148799+vigan7x@users.noreply.github.com>
2022-06-20 06:46:39 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const menuToggle = document.querySelector('.menu-toggle input')
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', () => {
+    nav.classList.toggle('slide')
+    console.log('first')
+})
